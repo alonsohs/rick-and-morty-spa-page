6c3d011f2762a251b3cd3de20516eaff35f3b0d8
Alonso Hernández Sastré <alonsohdez14@gmail.com>
2020-10-18 19:17:48 -0500

    First version

---
+++ src/utils/getData.js
@@ -0,0 +1,14 @@
+const API ='https://rickandmortyapi.com/api/character/'
+
+const getData = async (id) => {
+    const apiURl = id ? `${API}${id}` : API
+    try {
+        const response = await fetch(apiURl)
+        const data = await response.json()
+        return data
+    }catch (e) {
+        console.log('Fetch error', e)
+    }
+}
+
+export default getData
